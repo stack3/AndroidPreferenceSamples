6935acc96a108ef7e1033f2d2867e1f93079f707
MIYAMOTO, Hideaki <miyamoto@stack3.net>
2013-07-15 18:05:27 +0900

    Define minLength attribute.

--- src/net/stack3/preferencesamples/custompreference/CustomEditTextPreference.java
+++ src/net/stack3/preferencesamples/custompreference/CustomEditTextPreference.java
@@ -1,26 +1,42 @@
 package net.stack3.preferencesamples.custompreference;
 
+import net.stack3.preferencesamples.R;
 import android.content.Context;
 import android.preference.EditTextPreference;
 import android.util.AttributeSet;
+import android.widget.Toast;
 
 public class CustomEditTextPreference extends EditTextPreference {
+    private int minLength = Integer.MAX_VALUE;
+    
     public CustomEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setupAttributes(attrs);
     }
     
     public CustomEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setupAttributes(attrs);
     }
     
     public CustomEditTextPreference(Context context) {
         super(context);
     }
     
+    private void setupAttributes(AttributeSet attrs) {
+        minLength = attrs.getAttributeIntValue(
+                "http://schemas.android.com/apk/res/net.stack3.preferencesamples", 
+                "minLength", 
+                Integer.MAX_VALUE);
+    }
+    
     @Override
     protected boolean callChangeListener(Object newValue) {
         String newString = (String)newValue;
-        if (newString.length() < 3) {
+        if (newString.length() < minLength) {
+            String messageFormat = getContext().getString(R.string.too_short_string_length_format);
+            String message = String.format(messageFormat, minLength);
+            Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
             return false;
         } 
         
